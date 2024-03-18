da9cefe8ce232293f3be2c14a05d67becb734a25
muskanshah-7923 <124912381+muskanshah-7923@users.noreply.github.com>
2024-03-18 11:08:53 -0600

    start

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 // Array Visualizer Start
 
 // Global Variables
-let data = [1, 2, 3, 5, 8];
+let data = [1, 2, 3, 5, 8, 7, 4, 4, 1, 0, 8, 9, 10, 7];
 let maxVal = 10; // max data value
+
+// HTML Variables
+let outputEl = document.getElementById("container");
+
+// Traverse to Display Data Array
+
+function drawDatArray() {
+  for (let i = 0; i < data.length; i++) {}
+}
