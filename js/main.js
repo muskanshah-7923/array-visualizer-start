715faab4ca060545e87f019bd8d20e7d40721b8d
muskanshah-7923 <124912381+muskanshah-7923@users.noreply.github.com>
2024-03-19 11:22:56 -0600

    do more

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 // Array Visualizer Start
 
 // Global Variables
-let data = [1, 2, 3, 5, 8, 7, 4, 4, 1, 0, 8, 9, 10, 7];
-let maxVal = 10; // max data value
+let data = [];
+for (let n = 1; n <= 50; n++) {
+  data.push(randomInt(0, 101));
+}
+let maxVal = 100; // max data value
 
 // HTML Variables
 let outputEl = document.getElementById("container");
 
 // Traverse to Display Data Array
-
 function drawDatArray() {
-  for (let i = 0; i < data.length; i++) {}
+  let outputStr = "";
+  for (let val of data) {
+    let divHeight = (val / maxVal) * 600;
+    outputStr += `<div style="height:${divHeight}px"></div>`;
+  }
+  outputEl.innerHTML = outputStr;
 }
+
+// Draw Data Array every 200ms
+setInterval(drawDatArray, 200);
